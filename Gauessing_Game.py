35b91cef040dd22d2811aff714c54e20b4b18ff3
Thakuryogeshsingh <samil.yogesh@gmail.com>
2024-05-23 10:47:45 +0530

    First commit

---
+++ Gauessing_Game.py
@@ -0,0 +1,18 @@
+import random
+
+n = random.randrange(0,10)
+guess = int(input("Enter any number: "))
+while n!= guess:
+
+    if guess < n:
+        print("Too low")
+
+        guess = int(input("Enter number again: "))
+    elif guess > n:
+        print("Too high!")
+        guess = int(input("Enter number again: "))
+    else:
+        break
+    print("You guessed it right!!")
+
+    
